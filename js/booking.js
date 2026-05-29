7087ca3dad17c229984c5c0f0b64db7d0b9512ef
granthsahibsingh5007-sudo <granthsahibsingh5007@gmail.com>
2026-05-29 23:41:11 +0530

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,6 +1,21 @@
-alert("booking.js is working");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rishi Tours & Travels</title>
 
-document.getElementById("bookingForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("BOOK NOW clicked");
-});
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <h1>Welcome To Rishi Tours & Travels</h1>
+  <p>Luxury Cab Booking Service in Jamshedpur</p>
+
+  <img src="taxi.jpeg" width="300">
+
+  <button>Book Now</button>
+
+  <script src="js/booking.js"></script>
+</body>
+</html>
