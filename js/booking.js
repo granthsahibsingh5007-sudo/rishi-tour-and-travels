8c2486cfc2a66f8286a5a5216b1f6b914ccab2f5
granthsahibsingh5007-sudo <granthsahibsingh5007@gmail.com>
2026-05-30 21:45:23 +0530

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,39 +1,42 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e){
+document.getElementById("bookingForm").addEventListener("submit", async function(e){
 
     e.preventDefault();
 
     const bookingId =
-        "RTT" +
-        Math.floor(
-            1000 + Math.random() * 9000
-        );
+        "RTT" + Math.floor(1000 + Math.random() * 9000);
 
-    const name =
-        document.getElementById("name").value;
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const cabModel = document.getElementById("cabModel").value;
+    const tripType = document.getElementById("tripType").value;
+    const pickup = document.getElementById("pickup").value;
+    const drop = document.getElementById("drop").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
 
-    const phone =
-        document.getElementById("phone").value;
+    const { error } = await supabaseClient
+      .from("bookings")
+      .insert([
+        {
+          booking_id: bookingId,
+          customer_name: name,
+          customer_phone: phone,
+          cab_model: cabModel,
+          trip_type: tripType,
+          pickup_location: pickup,
+          drop_location: drop,
+          journey_date: date,
+          journey_time: time
+        }
+      ]);
 
-    const cabModel =
-        document.getElementById("cabModel").value;
+    if (error) {
+        alert("Database Save Failed");
+        console.log(error);
+        return;
+    }
 
-    const tripType =
-        document.getElementById("tripType").value;
-
-    const pickup =
-        document.getElementById("pickup").value;
-
-    const drop =
-        document.getElementById("drop").value;
-
-    const date =
-        document.getElementById("date").value;
-
-    const time =
-        document.getElementById("time").value;
-
-    const whatsappNumber =
-        "918651568297";
+    const whatsappNumber = "918651568297";
 
     const message =
 `🚖 Rishi Tours & Travels
@@ -65,15 +68,11 @@ ${date}
 ${time}`;
 
     const url =
-        `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+      https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)};
 
-    alert(
-        "Booking Created Successfully!\nBooking ID: " +
-        bookingId
-    );
+    alert("Booking Saved Successfully");
 
     window.open(url, "_blank");
 
     document.getElementById("bookingForm").reset();
-
 });
