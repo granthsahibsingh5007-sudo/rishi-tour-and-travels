2b394e05db328b48fc7ec0a0f68fd6194b3c0cf4
granthsahibsingh5007-sudo <granthsahibsingh5007@gmail.com>
2026-05-30 19:45:18 +0530

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,51 +1,79 @@
-document.querySelector("form").addEventListener("submit", function(e){
+document.getElementById("bookingForm").addEventListener("submit", function(e){
 
-e.preventDefault();
+    e.preventDefault();
 
-const name = document.getElementById("customerName").value;
-const mobile = document.getElementById("customerPhone").value;
-const vehicle = document.getElementById("cabType").value;
-const service = document.getElementById("serviceType").value;
-const pickup = document.getElementById("pickup").value;
-const drop = document.getElementById("drop").value;
-const date = document.getElementById("journeyDate").value;
-const time = document.getElementById("journeyTime").value;
+    const bookingId =
+        "RTT" +
+        Math.floor(
+            1000 + Math.random() * 9000
+        );
 
-if(!name || !mobile || !pickup || !drop){
-alert("Please fill all details");
-return;
-}
+    const name =
+        document.getElementById("name").value;
 
-const bookingId =
-"RTT-" +
-new Date().getFullYear() +
-"-" +
-Math.floor(1000 + Math.random() * 9000);
+    const phone =
+        document.getElementById("phone").value;
 
-const message =
-`🚖 Rishi Tours & Travels Booking
+    const cabModel =
+        document.getElementById("cabModel").value;
 
-🆔 Booking ID: ${bookingId}
-👤 Name: ${name}
-📞 Mobile: ${mobile}
-🚘 Vehicle: ${vehicle}
-🛣 Service: ${service}
-📍 Pickup: ${pickup}
-📍 Drop: ${drop}
-📅 Date: ${date}
-⏰ Time: ${time}`;
+    const tripType =
+        document.getElementById("tripType").value;
 
-const whatsappNumber = "918651568297";
+    const pickup =
+        document.getElementById("pickup").value;
 
-const url =
-`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+    const drop =
+        document.getElementById("drop").value;
 
-window.open(url, "_blank");
+    const date =
+        document.getElementById("date").value;
 
-alert(
-`Booking Submitted Successfully!
+    const time =
+        document.getElementById("time").value;
 
-Booking ID: ${bookingId}`
-);
+    const whatsappNumber =
+        "918651568297";
+
+    const message =
+`🚖 Rishi Tours & Travels
+
+Booking ID: ${bookingId}
+
+👤 Customer Name:
+${name}
+
+📞 Mobile:
+${phone}
+
+🚘 Cab Model:
+${cabModel}
+
+🛣️ Trip Type:
+${tripType}
+
+📍 Pickup:
+${pickup}
+
+📍 Drop:
+${drop}
+
+📅 Date:
+${date}
+
+⏰ Time:
+${time}`;
+
+    const url =
+        `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+
+    alert(
+        "Booking Created Successfully!\nBooking ID: " +
+        bookingId
+    );
+
+    window.open(url, "_blank");
+
+    document.getElementById("bookingForm").reset();
 
 });
