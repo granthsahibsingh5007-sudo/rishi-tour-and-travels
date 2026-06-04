fe334b58738f29c8e4c693ff242846532307529e
granthsahibsingh5007-sudo <granthsahibsingh5007@gmail.com>
2026-06-04 09:29:59 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -71,3 +71,67 @@ location.reload();
 }
 
 window.logout = logout;
+
+const SUPABASE_URL =
+"https://xgiezpajzzzhtgvrcagv.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_wdEbPt6FxlUNycufcS86kw_bglDaW5B";
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+
+async function loadBookings(){
+
+const table =
+document.getElementById("bookingTable");
+
+table.innerHTML =
+"<tr><td colspan='14'>Loading...</td></tr>";
+
+const { data, error } =
+await supabaseClient
+.from("bookings")
+.select("*")
+.order("id",{ascending:false});
+
+if(error){
+
+console.log(error);
+
+table.innerHTML =
+"<tr><td colspan='14'>Database Error</td></tr>";
+
+return;
+
+}
+
+table.innerHTML = "";
+
+data.forEach(item => {
+
+table.innerHTML += `
+<tr>
+<td>${item.booking_id || ""}</td>
+<td>${item.customer_name || ""}</td>
+<td>${item.customer_phone || ""}</td>
+<td>${item.cab_type || ""}</td>
+<td>${item.trip_type || ""}</td>
+<td>${item.pickup_location || ""}</td>
+<td>${item.drop_location || ""}</td>
+<td>${item.journey_date || ""}</td>
+<td>${item.journey_time || ""}</td>
+<td>${item.driver_name || "-"}</td>
+<td>${item.driver_phone || "-"}</td>
+<td>${item.vehicle_number || "-"}</td>
+<td>Manage</td>
+<td>WhatsApp</td>
+</tr>
+`;
+
+});
+
+}
